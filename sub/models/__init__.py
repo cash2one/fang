12f0b1ad4b9e4d5cde7f4d475155169a14aa190c
zrq495 <zrq495@gmail.com>
2017-04-06 15:46:42 +0800

    fixes

--- sub/models/__init__.py
+++ sub/models/__init__.py
@@ -1,1 +1,8 @@
 from .column import *  # noqa
+from .reply import *  # noqa
+from .post import *  # noqa
+from .action import *  # noqa
+from .active import *  # noqa
+from .article import *  # noqa
+from .voice import *  # noqa
+from .order import *  # noqa
