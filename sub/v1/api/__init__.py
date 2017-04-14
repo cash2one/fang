38ff715e09e18fc5a2604e565a5fe50875a9cec4
zrq495 <zrq495@gmail.com>
2017-04-14 11:37:57 +0800

    modified:   sub/v1/api/__init__.py

--- sub/v1/api/__init__.py
+++ sub/v1/api/__init__.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import
 import flask_restful as restful
 
 from ..validators import request_validate, response_filter
+from ..helpers import login_option
 
 
 class Resource(restful.Resource):
-    method_decorators = [request_validate, response_filter]
+    method_decorators = [request_validate, login_option, response_filter]
